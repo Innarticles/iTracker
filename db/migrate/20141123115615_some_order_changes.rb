94fe112ada8b0ed5dee4f873eb21897f40c8d05a
Innocent <innocentudeh@gmail.com>
2014-12-04 01:14:43 +0000

    experinmentig with bool

--- db/migrate/20141123115615_some_order_changes.rb
+++ db/migrate/20141123115615_some_order_changes.rb
@@ -1,8 +1,8 @@
 class SomeOrderChanges < ActiveRecord::Migration
   def up
     'integer USING CAST(column_name AS integer)'
-  	change_column("orders", "account_approval", 'boolean USING CAST(column_name AS boolean)')
-  	change_column("orders", "warehouse_approval", 'boolean USING CAST(column_name AS boolean')
+  	change_column("orders", "account_approval", 'boolean USING CAST(account_approval AS boolean)')
+  	change_column("orders", "warehouse_approval", 'boolean USING CAST(warehouse_approval AS boolean')
   	add_column("orders", "driver", :string, :after => "destination")
   	
   end
